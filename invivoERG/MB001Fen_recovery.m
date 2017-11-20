770954cfe752dfb25b3219101700d72bb0270b95
trucho <angueyra@gmail.com>
2017-11-19 19:50:04 -0500

    more cells for recovery

--- invivoERG/MB001Fen_recovery.m
+++ invivoERG/MB001Fen_recovery.m
@@ -1,31 +1,4 @@
 % screening (1 by 1 unfortunately)
-
-%% MB001 High dose
-%Sq1040
-dirData = '20171023/20171023_Sq1040_MB001High';
-dirFile = '02_FlashesPre';
-dirFile = '03_FlashesPost0s';
-dirFile = '04_FlashesPost1min09s';
-dirFile = '05_FlashesPost2min38s';
-dirFile = '06_FlashesPost4min07s';
-dirFile = '07_FlashesPost5min35s';
-dirFile = '08_FlashesPost7min05s';
-dirFile = '09_FlashesPost8min34s';
-dirFile = '11_FlashesPost14min29s';
-dirFile = '12_FlashesPost16min12s';
-
-dirData = '20171025/20171025_Sq1040_MB001High';
-dirFile = '02_FlashPre';
-dirFile = '03_FlashPost0s';
-dirFile = '04_FlashPost1min19s';
-dirFile = '05_FlashPost2min53s';
-dirFile = '06_FlashPost4min20s';
-dirFile = '07_FlashPost5min48s';
-dirFile = '08_FlashPost7min14s';
-dirFile = '09_FlashPost8min37s';
-dirFile = '11_FlashPost13min55s';
-dirFile = '12_FlashPost15min23s';
-
 %% Vehicle
 %Sq999
 % dirData = '20170905/20170905_Sq999_Vehicle';
@@ -77,21 +50,21 @@ dirFile = '12_FlashPost15min23s';
 % dirFile = '12_FlashesPost8min30s';
 
 %Sq992
-dirData = '20170830/20170830_Sq992_Veh';
-dirFile = '02_FlashesPre';
-dirFile = '03_FlashesPost0s';
-dirFile = '04_FlashesPost1min10s';
-dirFile = '05_FlashesPost2min10s';
-dirFile = '06_FlashesPost3min5s';
-dirFile = '07_FlashesPost4min';
-dirFile = '08_FlashesPost5min10s';
-dirFile = '09_FlashesPost6min20s';
-dirFile = '10_FlashesPost7min40s';
-dirFile = '11_FlashesPost8min45s';
-% 
-% 
-dirData = '20170901/20170901_Squirrel992_Vehicle';
-dirFile = '02_FlashesPre';
+% dirData = '20170830/20170830_Sq992_Veh';
+% dirFile = '02_FlashesPre';
+% dirFile = '03_FlashesPost0s';
+% dirFile = '04_FlashesPost1min10s';
+% dirFile = '05_FlashesPost2min10s';
+% dirFile = '06_FlashesPost3min5s';
+% dirFile = '07_FlashesPost4min';
+% dirFile = '08_FlashesPost5min10s';
+% dirFile = '09_FlashesPost6min20s';
+% dirFile = '10_FlashesPost7min40s';
+% dirFile = '11_FlashesPost8min45s';
+% % 
+% % 
+% dirData = '20170901/20170901_Squirrel992_Vehicle';
+% dirFile = '02_FlashesPre';
 % dirFile = '03_FlashesPost0s';
 % dirFile = '04_FlashesPost1min20s';
 % dirFile = '05_FlashesPost2min45s';
@@ -100,10 +73,180 @@ dirFile = '02_FlashesPre';
 % dirFile = '08_FlashesPost7min06s';
 % dirFile = '09_FlashesPost8min40s';
 % dirFile = '11_FlashesPost13min50s';
+%% MB001 High dose
+% %Sq1040
+% dirData = '20171023/20171023_Sq1040_MB001High';
+% dirFile = '02_FlashesPre';
+% dirFile = '03_FlashesPost0s';
+% dirFile = '04_FlashesPost1min09s';
+% dirFile = '05_FlashesPost2min38s';
+% dirFile = '06_FlashesPost4min07s';
+% dirFile = '07_FlashesPost5min35s';
+% dirFile = '08_FlashesPost7min05s';
+% dirFile = '09_FlashesPost8min34s';
+% dirFile = '11_FlashesPost14min29s';
+% dirFile = '12_FlashesPost16min12s';
+% 
+% dirData = '20171025/20171025_Sq1040_MB001High';
+% dirFile = '02_FlashPre';
+% dirFile = '03_FlashPost0s';
+% dirFile = '04_FlashPost1min19s';
+% dirFile = '05_FlashPost2min53s';
+% dirFile = '06_FlashPost4min20s';
+% dirFile = '07_FlashPost5min48s';
+% dirFile = '08_FlashPost7min14s';
+% dirFile = '09_FlashPost8min37s';
+% dirFile = '11_FlashPost13min55s';
+% dirFile = '12_FlashPost15min23s';
 
+%Sq928
+% dirData = '20170830/20170830_Sq928_MB001High';
+% dirFile = '03_FlashesPre';
+% dirFile = '04_FlashesPost0s';
+% dirFile = '05_FlashesPost40s';
+% dirFile = '06_FlashesPost2min';
+% dirFile = '07_FlashesPost3min10s';
+% dirFile = '08_FlashesPost4min30s';
+% dirFile = '09_FlashesPost6min';
+% dirFile = '10_FlashesPost7min20s';
+% dirFile = '11_FlashesPost8min30s';
+% % 
+% dirData = '20170901/20170901_Squirrel928_MB001High';
+% dirFile = '03_FlashesBrighterPre';
+
+%Sq1006
+% dirData = '20170829/20170829_Sq1006_MB001High';
+% dirFile = '04_FlashesPre';
+% dirFile = '05_FlashesPost0s';
+% dirFile = '06_FlashesPost1min';
+% dirFile = '07_FlashesPost2min20s';
+% dirFile = '08_FlashesPost4min30s';
+% dirFile = '09_FlashesPost6min30s';
+% dirFile = '10_FlashesPost7min50s';
+% % 
+% % 
+% dirData = '20170831/20170831_Sq1006_MB001High';
+% dirFile = '02_FlashesPre';
+% dirFile = '03_FlashesPost0s';
+% dirFile = '04_FlashesPost40s';
+% dirFile = '05_FlashesPost2min';
+% dirFile = '06_FlashesPost2min55s';
+% dirFile = '07_FlashesPost3min50s';
+% dirFile = '08_FlashesPost4min50s';
+% dirFile = '09_FlashesPost5min45s';
+% dirFile = '10_FlashesPost6min43s';
+% dirFile = '11_FlashesPost7min40s';
+% dirFile = '12_FlashesPost8min40s';
+% dirFile = '16_FlashesPost18min';
+%% MB001 Low
+%Sq993
+% dirData = '20170829/20170829_Sq993_MB001Low';
+% dirFile = '03_FlashesPre';
+% dirFile = '04_FlashesPost0s';
+% dirFile = '05_FlashesPost1min';
+% dirFile = '06_FlashesPost2min30s';
+% dirFile = '07_FlashesPost3min55s';
+% dirFile = '08_FlashesPost5min';
+% dirFile = '09_FlashesPost6min50s';
+% dirFile = '10_FlashesPost8min';
+% % 
+% % 
+% dirData = '20170831/20170831_Sq993_MB001Low';
+% dirFile = '02_FlashesPre';
+% dirFile = '03_FlashesPost0s';
+% dirFile = '04_FlashesPost40s';
+% dirFile = '05_FlashesPost1min30s';
+% dirFile = '06_FlashesPost2min25s';
+% dirFile = '07_FlashesPost3min30s';
+% dirFile = '08_FlashesPost4min30s';
+% dirFile = '09_FlashesPost5min28s';
+% dirFile = '10_FlashesPost6min25s';
+% dirFile = '11_FlashesPost7min27s';
+% dirFile = '12_FlashesPost8min20s';
+% dirFile = '13_FlashesPost9min20s';
+% dirFile = '15_FlashesPost14min20s';
+
+%Sq998
+% dirData = '20170830/20170830_Sq998_MB001Low';
+% dirFile = '02_FlashesPre';
+% dirFile = '03_FlashesPost0s';
+% dirFile = '04_FlashesPost40s';
+% dirFile = '05_FlashesPost1min20s';
+% dirFile = '06_FlashesPost2min30s';
+% dirFile = '07_FlashesPost3min50s';
+% dirFile = '08_FlashesPost4min55s';
+% dirFile = '09_FlashesPost6min10s';
+% dirFile = '10_FlashesPost7min30s';
+% dirFile = '11_FlashesPost8min40s';
+% % 
+% % 
+% dirData = '20170901/20170901_Squirrel998_MB001Low';
+% dirFile = '02_FlashesPre';
+% dirFile = '03_FlashesPost0s';
+% dirFile = '04_FlashesPost1min17s';
+% dirFile = '05_FlashesPost2min45s';
+% dirFile = '06_FlashesPost4min06s';
+% dirFile = '07_FlashesPost5min32s';
+% dirFile = '08_FlashesPost6min57s';
+% dirFile = '09_FlashesPost8min25s';
+% dirFile = '11_FlashesPost13min50s';
+% dirFile = '12_FlashesPost15min40s';
+% dirFile = '13_FlashesPost17min0s';
+
+%% Fenretinide
+%Sq990
+% dirData = '20170905/20170905_Sq990_Fenretinide';
+% dirFile = '02_FlashesPre';
+% dirFile = '03_FlashesPost0s';
+% dirFile = '04_FlashesPost1min17s';
+% dirFile = '05_FlashesPost2min40s';
+% dirFile = '06_FlashesPost4min05s';
+% dirFile = '07_FlashesPost5min36s';
+% dirFile = '08_FlashesPost7min06s';
+% dirFile = '09_FlashesPost8min28s';
+% dirFile = '11_FlashesPost13min52s';
+% dirFile = '12_FlashesPost15min52s';
+% 
+% 
+% dirData = '20170907/20170907_Sq990_Fenretinide';
+% dirFile = '02b_FlashesPre';
+% dirFile = '03_FlashesPost0s';
+% dirFile = '04_FlashesPost1min25s';
+% dirFile = '05_FlashesPost2min50s';
+% dirFile = '06_FlashesPost4min20s';
+% dirFile = '07_FlashesPost5min45s';
+% dirFile = '08_FlashesPost7min11s';
+% dirFile = '09_FlashesPost8min41s';
+% dirFile = '11_FlashesPost13min43s';
+% dirFile = '12_FlashesPost15min12s';
+
+%Sq995
+dirData = '20170905/20170905_Sq995_Fenretinide';
+dirFile = '02_FlashesPre';
+dirFile = '03_FlashesPost0s';
+dirFile = '04_FlashesPost1min23s';
+% dirFile = '05_FlashesPost2min27s';
+% dirFile = '06_FlashesPost4min16s';
+% dirFile = '07_FlashesPost5min44s';
+% dirFile = '08_FlashesPost7min08s';
+% dirFile = '09_FlashesPost8min36s';
+% dirFile = '11_FlashesPost13min47s';
+% dirFile = '12_FlashesPost15min20s';
+% % 
+% dirData = '20170907/20170907_Sq995_Fenretinide';
+% dirFile = '02_FlashesPre';
+% dirFile = '03_FlashesPost0s';
+% dirFile = '04_FlashesPost1min11s';
+% dirFile = '05_FlashesPost2min37s';
+% dirFile = '06_FlashesPost4min03s';
+% dirFile = '07_FlashesPost5min28s';
+% dirFile = '08_FlashesPost6min53s';
+% dirFile = '09_FlashesPost8min20s';
+% dirFile = '11_FlashesPost13min42s';
+% dirFile = '12_FlashesPost15min10s';
+% dirFile = '13_FlashesPost16min36s';
 
 erg=ERGobj(dirData,dirFile);
-
 % %% first screen trials
 hGUI=erg_screenrecovery(erg,[],10);
 % set(hGUI.figH,'Position',[-1760 -43 1572 989])
@@ -114,13 +257,15 @@ hGUI=erg_screenrecovery(erg,[],10);
 a=[...
     0,0;...
     0,0;...
-    1,20;...
-    2,45;...
-    4,15;...
-    5,40;...
-    7,06;...
-    8,40;...
-    13,50;...
+    1,11;...
+    2,37;...
+    4,03;...
+    5,28;...
+    6,53;...
+    8,20;...
+    13,42;...
+    15,10;...
+    16,36;...
 ];
 
 % a=[...
@@ -146,9 +291,20 @@ fprintf(']\n')
 
 
 %% concatenator
-whichone='Sq1040';
+% %MB001 High
+% whichone='Sq1006';
+% whichone='Sq1040';
+% whichone='Sq928';
+% %MB001 Low
+% whichone='Sq993';
+% whichone='Sq998';
+% %Fenretinide
+whichone='Sq990';
+whichone='Sq995';
+% whichone='Sq990';
+% %Vehicle
 % whichone='Sq999';
-whichone='Sq1000';
+% whichone='Sq1000';
 % whichone='Sq992';
 ergR=erg_recovery(whichone);
 % title('MB001')
